f4d6a47eb01a141bd09fc6d665a20380b0b615a7
天夜Yoku <mamaitoto0206@gmail.com>
2021-03-06 11:06:32 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,8 +9,7 @@ intens = discord.Intents.all()
 with open("settings.json", mode= 'r', encoding= 'utf8') as jfile:
     jdata = json.load(jfile)
 
-with open('data/Token.0', mode='r', encoding='utf8') as tfile:
-    Token = tfile.read()
+Toktn = os.getenv("Token")
 
 
 def time_get():
