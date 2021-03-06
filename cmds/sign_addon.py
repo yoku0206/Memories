a9c6a52dd5779b265d378eecffb151eee337c65a
天夜Yoku <mamaitoto0206@gmail.com>
2021-03-06 11:31:04 +0800

    main

--- cmds/sign_addon.py
+++ cmds/sign_addon.py
@@ -21,19 +21,27 @@ class Sign_Addon(Cog_Extension):
 
         temp = {}
 
-        num = (page - 1) * 10
-        num_end = page * 10
-
         for i in gdata[str(ctx.guild.id)]['user']:
             temp[i] = gdata[str(ctx.guild.id)]['user'][i]['total']
 
         data = sorted(temp.items(), key=lambda x:x[1], reverse=True)
 
+        if page > len(data) // 10:
+            page = len(data) // 10
+        else:
+            pass
+        num = (page - 1) * 10
+        num_end = page * 10
+
+
         embed=discord.Embed(title=f"{ctx.guild.name}", description=f"**簽到天數排行榜** {num + 1} ～ {num_end}", color=ctx.author.color, timestamp=Setting.time_get(self))
         embed.set_thumbnail(url=ctx.guild.icon_url)
         for i in range(num, num_end):
-            member = await MemberConverter().convert(ctx, data[i][0])
-            embed.add_field(name=f"{i + 1}. {member.name}#{member.discriminator} ", value=f"總共簽到： **{data[i][1]}** 天", inline=False)
+            try:
+                member = await MemberConverter().convert(ctx, data[i][0])
+                embed.add_field(name=f"{i + 1}. {member.name}#{member.discriminator} ", value=f"總共簽到： **{data[i][1]}** 天", inline=False)
+            except:
+                pass
         message = await ctx.send(embed=embed)
         await asyncio.sleep(60)
         await message.delete()
